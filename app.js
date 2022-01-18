1f170a045f9057dbaf19a0a3cef34681b37eb654
Zer0cheros <christian.wiss@optimaedu.fi>
2022-01-18 20:33:59 +0200

    init

---
+++ app.js
@@ -0,0 +1,9 @@
+function showName(name){
+    const root = document.createElement('div')
+    const text = document.createElement('h2')
+    text.innerHTML = name + ' was here!'
+    root.appendChild(text)
+    document.body.appendChild(root)
+}
+//Christian
+showName('Christian')
